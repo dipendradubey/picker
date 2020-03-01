7b025eff5c7d4e443ec72b05e03014aa20b0f6ff
Dipendra <dipendradubey@gmail.com>
2020-03-01 19:40:09 +0530

    Finished created the picker framework

---
+++ picker/picker/Service.swift
@@ -0,0 +1,16 @@
+//
+//  Service.swift
+//  picker
+//
+//  Created by Dipendra Dubey on 01/03/20.
+//  Copyright © 2020 Dipendra. All rights reserved.
+//
+
+import Foundation
+
+public class Service {
+    private init(){}
+    public static func serviceName()->String{
+        return "abc"
+    }
+}
---
+++ picker/picker/picker.h
@@ -0,0 +1,19 @@
+//
+//  picker.h
+//  picker
+//
+//  Created by Dipendra Dubey on 01/03/20.
+//  Copyright © 2020 Dipendra. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for picker.
+FOUNDATION_EXPORT double pickerVersionNumber;
+
+//! Project version string for picker.
+FOUNDATION_EXPORT const unsigned char pickerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <picker/PublicHeader.h>
+
+
